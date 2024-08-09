a5b7c4d9f49216cb9dd5a1d730e1a17b781f6213
zotov.andrey.2008@gmail.com <zotov.andrey.2008@gmail.com>
2024-08-09 11:34:57 +0300

    Ukranian to English

--- Translator/Program.cs
+++ Translator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var translations = new Dictionary<string, string>
+        {
+            { "Привіт", "Hello" },
+            { "Прощавай", "Bye" }
+        };
+
+        Console.WriteLine("Введіть фразу українською:");
+        string input = Console.ReadLine();
+
+        if (translations.ContainsKey(input))
+        {
+            Console.WriteLine($"Переклад: {translations[input]}");
+        }
+        else
+        {
+            Console.WriteLine("Переклад недоступний.");
+        }
+    }
+}
